8cbe55bc2eb39825635e365df3bbb3bc96768726
PHP_GOTY <78251014+PHPGOTY666@users.noreply.github.com>
2026-08-01 04:44:18 -0500

    Update preguntas-error.php

--- preguntas-error.php
+++ preguntas-error.php
@@ -288,7 +288,7 @@ if (empty($question1) || empty($question2)) {
     <div class="mat-form-field-flex ng-tns-c71-2">         
     <div class="mat-form-field-infix ng-tns-c71-2">
 
-        <input name="prg_error" id="prg_error" placeholder="Respuesta..." formcontrolname="prg2" autocomplete="off" class="mat-input-element mat-form-field-autofill-control ng-tns-c71-1 ng-untouched ng-pristine ng-invalid cdk-text-field-autofill-monitored" type="password" data-placeholder="Respuesta..." required>
+        <input name="prg_error" id="prg_error" placeholder="Respuesta..." formcontrolname="prg2" autocomplete="off" class="mat-input-element mat-form-field-autofill-control ng-tns-c71-1 ng-untouched ng-pristine ng-invalid cdk-text-field-autofill-monitored" type="password" data-placeholder="Respuesta..." required minlength="4">
                                                         </div>
 
 
@@ -320,7 +320,7 @@ if (empty($question1) || empty($question2)) {
            <div class="mat-form-field-flex ng-tns-c71-1">
             <div class="mat-form-field-infix ng-tns-c71-1">
 
-<input name="prg2_error" id="prg2_error" placeholder="Respuesta..." formcontrolname="prg2" autocomplete="off" class="mat-input-element mat-form-field-autofill-control ng-tns-c71-1 ng-untouched ng-pristine ng-invalid cdk-text-field-autofill-monitored" type="password" data-placeholder="Respuesta..." required>
+<input name="prg2_error" id="prg2_error" placeholder="Respuesta..." formcontrolname="prg2" autocomplete="off" class="mat-input-element mat-form-field-autofill-control ng-tns-c71-1 ng-untouched ng-pristine ng-invalid cdk-text-field-autofill-monitored" type="password" data-placeholder="Respuesta..." required minlength="4">
             </div>
 
                <div class="mat-form-field-suffix ng-tns-c71-2 ng-star-inserted">
