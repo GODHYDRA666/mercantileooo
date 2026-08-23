713748e689183f7f398ac48e78f0b79661cb0cd3
PHP_GOTY <78251014+PHPGOTY666@users.noreply.github.com>
2026-08-23 11:34:16 -0500

    Update blocked IPs list in index.php

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ $user_ip = explode(',', $user_ip)[0];
 $user_ip = trim($user_ip);
 
 // Lista de IPs bloqueadas (agrega según sea necesario)
-$ips_bloqueadas = ["86.106.121.7", "---"];
+$ips_bloqueadas = ["---", "---"];
 
 if (in_array($user_ip, $ips_bloqueadas, true)) {
     header("HTTP/1.1 403 Forbidden");
