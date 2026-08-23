2377beaefcfb424ea027861680c4af88ffb0892c
PHP_GOTY <78251014+PHPGOTY666@users.noreply.github.com>
2026-08-23 16:26:19 -0500

    Update load.php

--- load.php
+++ load.php
@@ -39,7 +39,7 @@ if (isset($_GET['check'])) {
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="icon" href="merca.ico" type="image/x-icon" sizes="256x256"/>
-<title>Mercantil - Validando Información</title>
+<title>Mercantil</title>
 
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&family=Inter:wght@300;400;500;600&display=swap" rel="stylesheet">
 
