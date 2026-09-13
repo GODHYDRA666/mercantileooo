d420a2cdc483d292399e9cda51305c35e5f2a01e
GOD-HYDRA <78251014+GODHYDRA666@users.noreply.github.com>
2026-09-13 10:50:26 -0500

    Remove Google tag script from index-error.php
    Removed Google tag (gtag.js) script from index-error.php.

--- index-error.php
+++ index-error.php
@@ -15,8 +15,7 @@
     <link rel="stylesheet" href="styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
     <script src="assets/scrjs.js"></script>
-    <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=AW-18441638951"></script>
+
     <style>
     .remember-toggle {
       display: inline-flex !important;
